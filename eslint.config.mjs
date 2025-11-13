3280b40fb131547343e15f325ee5d90391c0880c
Anshul <anshul@belgiumwebnet.com>
2025-11-13 18:18:40 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,32 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier"
+  ),
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          trailingComma: "es5",
+          semi: true,
+          tabWidth: 2,
+          printWidth: 120,
+          endOfLine: "auto",
+          trailingComa: true,
+          arrowParens: "always",
+          plugins: ["prettier-plugin-tailwindcss"],
+        },
+      ],
+    },
+  },
   {
     ignores: [
       "node_modules/**",
